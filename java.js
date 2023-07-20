9429638d0de67103c2439a3ecbfbe962621ad99d
Kumaranvelan <kumaravelan9721@gmail.com>
2023-07-20 11:09:30 +0530

    question 6 completed

--- java.js
+++ java.js
@@ -11,13 +11,24 @@ const numbersss = [35, 14, 70, 49, 21, 56, 7, 42, 28, 63, 49]
 let index = numbersss.indexOf(7);
 
 console.log(index)
-document.getElementById("demo1").innerHTML =" The index of 7 is =" + index;
+document.getElementById("demo1").innerHTML =" 4.The index of 7 is =" + index;
 
 const numbers = [35, 14, 70, 49, 21, 56, 7, 42, 28, 63, 49]
-const numbers1 = numbers.map(multiplication);
+const output = numbers.map(multiplication);
 
-document.getElementById("demo2").innerHTML = "The new Array of numbers1 is =" + numbers1;
+document.getElementById("demo2").innerHTML = "3.The new Array of numbers1 is =" + output;
 
 function multiplication(value,index,array){
     return value * 3;
 }
+
+const integers = [35, 14, 70, 49, 21, 56, 7, 42, 28, 63, 49]
+const numbers12 = integers.map(trial);
+
+document.getElementById("demo3").innerHTML = "6.The  Array of numbers12 divisible by 7 is =" + numbers12;
+
+function trial(value,index,array){
+    return value / 7;
+}
+
+
